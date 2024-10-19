69b5f845e98274813751d5bbba65f34126196205
Holence <Holence08@gmail.com>
2024-10-19 22:36:50 +0800

    i dont like x86

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -2,25 +2,51 @@
 #include <string.h>
 
 int64_t asm_add(int64_t a, int64_t b) {
-  return a + b;
+    asm("add %1, %0"
+        : "+r"(b)
+        : "r"(a));
+    return b;
 }
 
 int asm_popcnt(uint64_t x) {
-  int s = 0;
-  for (int i = 0; i < 64; i++) {
-    if ((x >> i) & 1) s++;
-  }
-  return s;
+    uint64_t res;
+    asm("popcnt %1, %0"
+        : "=r"(res)
+        : "r"(x));
+    return res;
 }
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
-  return memcpy(dest, src, n);
+    uint8_t temp = 0;
+    asm(
+        "loop:"
+        "cmp $0x0, %[n];"
+        "je done;"
+        "dec %[n];"
+        "mov (%[src], %[n], 1), %[temp];"
+        "mov %[temp], (%[dest], %[n], 1);"
+        "jmp loop;"
+        "done:"
+        :
+        : [temp] "r"(temp), [dest] "r"(dest), [src] "r"(src), [n] "r"(n));
+
+    // magic
+    // asm volatile(
+    //     "rep movsb"   // Use the `rep movsb` instruction to move `n` bytes
+    //     :             // No output operands (memory is modified implicitly)
+    //     : "D"(dest),  // `D` is the destination pointer (rdi register)
+    //       "S"(src),   // `S` is the source pointer (rsi register)
+    //       "c"(n)      // `c` is the count of bytes to move (rcx register)
+    //     : "memory"    // Clobbers memory
+    // );
+
+    return dest;
 }
 
 int asm_setjmp(asm_jmp_buf env) {
-  return setjmp(env);
+    return setjmp(env);
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
-  longjmp(env, val);
+    longjmp(env, val);
 }
